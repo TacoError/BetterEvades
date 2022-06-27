da71c765e016f6d99bac36ae3bb34e8a024426f8
TacoError <radltaylor14@gmail.com>
2022-06-26 18:43:29 -0700

    missed summn

--- server/physics.js
+++ server/physics.js
@@ -7,7 +7,7 @@ module.exports = {
                 w.sat = new SAT.Box(new SAT.Vector(w.x, w.y), w.width, w.height).toPolygon();
             }
             // TODO: globally generate a p.sat every frame after movement for all collision functions to use
-            p.sat = new SAT.Circle(new SAT.Vector(p.x,p.y),p.radius);
+            p.sat = new SAT.Circle(new SAT.Vector(p.x, p.y),p.radius);
             const response = new SAT.Response();
             const collided = SAT.testPolygonCircle(w.sat, p.sat, response);
             if (collided) {
